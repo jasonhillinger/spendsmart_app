cd05e3a5afa065ee6fc9cb14ffa5a08efe693522
CSman22 <jackey211@hotmail.com>
2023-04-15 03:55:24 -0400

    Created popup menu for the balance card

--- app/src/main/java/com/example/spendsmart_soen357_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/spendsmart_soen357_app/ui/home/HomeFragment.java
@@ -1,15 +1,21 @@
 package com.example.spendsmart_soen357_app.ui.home;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.spendsmart_soen357_app.R;
 import com.example.spendsmart_soen357_app.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -26,7 +32,93 @@ public class HomeFragment extends Fragment {
 
         //final TextView textView = binding.textHome;
         //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Button month = binding.btnMonth;
+        month.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showMenu(getContext(),view, R.menu.balance_menu);
+            }
+        });
+
         return root;
+    }
+
+    private void showMenu(Context context, View v, int balance_menu) {
+        PopupMenu popup = new PopupMenu(getContext(), v);
+        popup.getMenuInflater().inflate(balance_menu, popup.getMenu());
+
+        // Retrieve the state of the checkable item from SharedPreferences
+        SharedPreferences menuPref = context.getSharedPreferences(getString(R.string.balance_menu), Context.MODE_PRIVATE);
+        boolean thisMonthSelected = menuPref.getBoolean("menu_item_this_month", false);
+        boolean threeMonthSelected = menuPref.getBoolean("menu_item_3_month", false);
+        boolean sixMonthSelected = menuPref.getBoolean("menu_item_6_month", false);
+        boolean yearSelected = menuPref.getBoolean("menu_item_year", false);
+        if (thisMonthSelected)
+            popup.getMenu().findItem(R.id.this_month).setChecked(thisMonthSelected);
+        else if (threeMonthSelected) {
+            popup.getMenu().findItem(R.id.past_3_month).setChecked(threeMonthSelected);
+        } else if (sixMonthSelected) {
+            popup.getMenu().findItem(R.id.past_6_month).setChecked(sixMonthSelected);
+        } else if (yearSelected) {
+            popup.getMenu().findItem(R.id.past_year).setChecked(yearSelected);
+        }
+
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.balance_menu),Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                // Handle item selection
+                switch (menuItem.getItemId()) {
+                    case R.id.this_month:
+                        menuItem.setChecked(true);
+                        editor.putBoolean("menu_item_this_month",true);
+                        editor.putBoolean("menu_item_3_month",false);
+                        editor.putBoolean("menu_item_6_month",false);
+                        editor.putBoolean("menu_item_year",false);
+                        editor.apply();
+                        return true;
+                    case R.id.past_3_month:
+                        menuItem.setChecked(true);
+                        editor.putBoolean("menu_item_this_month",false);
+                        editor.putBoolean("menu_item_3_month",true);
+                        editor.putBoolean("menu_item_6_month",false);
+                        editor.putBoolean("menu_item_year",false);
+                        editor.apply();
+                        return true;
+                    case R.id.past_6_month:
+                        menuItem.setChecked(true);
+                        editor.putBoolean("menu_item_this_month",false);
+                        editor.putBoolean("menu_item_3_month",false);
+                        editor.putBoolean("menu_item_6_month",true);
+                        editor.putBoolean("menu_item_year",false);
+                        editor.apply();
+                        return true;
+                    case R.id.past_year:
+                        menuItem.setChecked(true);
+                        editor.putBoolean("menu_item_this_month",false);
+                        editor.putBoolean("menu_item_3_month",false);
+                        editor.putBoolean("menu_item_6_month",false);
+                        editor.putBoolean("menu_item_year",true);
+                        editor.apply();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        popup.setOnDismissListener(new PopupMenu.OnDismissListener() {
+            @Override
+            public void onDismiss(PopupMenu menu) {
+                // Respond to popup being dismissed.
+            }
+        });
+
+        // Show the popup menu.
+        popup.show();
     }
 
     @Override
